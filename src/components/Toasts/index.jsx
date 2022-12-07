c4a374ecd760a6be9352e50d49ef8eb66b4c1007
Hieu Nguyen <nhocproaoe@gmail.com>
2022-12-07 23:44:29 +0700

    add toast when add to cart

---
+++ src/components/Toasts/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+function Toasts(props) {
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ToastContainer />
+        </>
+    );
+}
+
+export default Toasts;
