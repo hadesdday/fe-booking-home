53cd4987080e9ac32495e857188b60fa6d96b7a7
chanhhiep <19130067@st.hcmuaf.edu.vn>
2022-12-13 00:56:52 +0700

    overview of list home

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.scss";
 import AppProvider from "./context/AppContext";
 import Login from "./pages/Account/Login";
 import Register from "./pages/Account/Register";
+import Overview from "./pages/Listing/Overview";
 import ResetPassword from "./pages/Account/ResetPassword";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
@@ -48,6 +49,7 @@ function App() {
             <Route path="Login" element={<Login />} />
             <Route path="Register" element={<Register />} />
             <Route path="ResetPassword" element={<ResetPassword />} />
+            <Route path="Overview" element={<Overview />} />
             <Route path="/home/details/:id" element={<HomeDetails />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
