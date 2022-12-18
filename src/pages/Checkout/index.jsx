fea1b99cfe81ee8dbe69e730bf34688a19d74192
Hieu Nguyen <nhocproaoe@gmail.com>
2022-12-19 00:18:42 +0700

    fill data in checkout page

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -11,8 +11,65 @@ import "./styles.scss";
 
 function Checkout(props) {
     const { selectedDayRange, home, country, setCountry } = useContext(AppContext);
+
     const countryObject = COUNTRY_LIST.find(a => a.value === country);
     const night = getNightNumber(selectedDayRange);
+    console.log(selectedDayRange);
+    const chosenItems = JSON.parse(sessionStorage.getItem("chosenItem"));
+    console.log(chosenItems);
+
+    function getDateInObject(date) {
+        const re = {
+            year: 0,
+            month: 0,
+            day: 0
+        }
+        const d = new Date(date);
+        re.year = d.getFullYear();
+        re.month = d.getMonth() + 1;
+        re.day = d.getDate();
+        return re;
+    }
+
+    function getAvgRating(ratings) {
+        if (ratings.length === 0) {
+            return 0;
+        }
+        let sum = 0;
+        ratings.map((i) => sum += i.rate);
+        return sum / ratings.length;
+    }
+
+    function getRatingElements(ratings) {
+        let re = [];
+        const avg = getAvgRating(ratings);
+        for (let i = 1; i <= avg; i++) {
+            re.push(<i className='bx bxs-star text-warning' key={i}></i>);
+        }
+        return re;
+    }
+
+    function getDateRangeFromObject(fr, t) {
+        const from = getDateInObject(fr);
+        const to = getDateInObject(t);
+        return { from, to };
+    }
+
+    function getCapacity(rooms) {
+        if (rooms.length === 0) return 0;
+        const numberOfPeople = rooms.length > 0 && rooms.reduce((a, b) => ({ children: a.children + b.children, adult: a.adult + b.adult }));
+        const capacity = numberOfPeople && numberOfPeople.children + numberOfPeople.adult;
+        return capacity;
+    }
+
+    var totalPrice = 0;
+    if (chosenItems.length > 0) {
+        chosenItems.map((item) => totalPrice += item.hotel.rooms[0].price);
+    }
+
+    var fee = totalPrice * 0.1;
+    var total = totalPrice + fee;
+
 
     return (
         <>
@@ -77,98 +134,54 @@ function Checkout(props) {
                                 <div className="bg-light-green rounded-top p-3 border-bottom">
                                     <h4 className='fw-semibold'>Booking Summary</h4>
                                 </div>
-                                <div className="border-bottom">
-                                    <div className="row pt-3">
-                                        <p className='fw-semibold'><i className='bx bxs-building-house'></i>HOTEL <span className="fs-70 text-muted">(HO CHI MINH CITY)</span></p>
-                                    </div>
-                                    <div className="row g-0 m-2">
-                                        <div className="col-3">
-                                            <img src="https://picsum.photos/150/150" alt="" width={80} className="rounded-1" />
+                                {chosenItems && chosenItems.map((item, index) => (
+                                    <div className="border-bottom" key={index}>
+                                        <div className="row pt-3">
+                                            <p className='fw-semibold'><i className='bx bxs-building-house'></i>HOTEL <span className="fs-70 text-muted">({item.hotel.place.province})</span></p>
                                         </div>
-                                        <div className="col-9">
-                                            <h5 className='fw-semibold'>NTA Serviced Apartments</h5>
-                                            <div>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
+                                        <div className="row g-0 m-2">
+                                            <div className="col-3">
+                                                <img src={item.hotel.images[0].image} alt="" width={80} className="rounded-1" />
                                             </div>
-                                            <div className="row">
-                                                <div className="col-2 bg-main text-center rounded-2 p-1 m-2 mx-wh-30 fw-semibold text-white">10</div>
-                                                <div className="col-10 p-0 mt-1">
-                                                    <p className='m-0 fw-semibold fs-smaller'>Exceptional</p>
-                                                    <p className='fs-smaller'>From 300 reviews</p>
+                                            <div className="col-9">
+                                                <h5 className='fw-semibold'>{item.hotel.name}</h5>
+                                                <div>
+                                                    {getRatingElements(item.hotel.reviews)}
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-2 bg-main text-center rounded-2 p-1 m-2 mx-wh-30 fw-semibold text-white">{getAvgRating(item.hotel.reviews)}</div>
+                                                    <div className="col-10 p-0 mt-1">
+                                                        <p className='m-0 fw-semibold fs-smaller'>Exceptional</p>
+                                                        <p className='fs-smaller'>From {item.hotel.reviews.length} review{item.hotel.reviews.length && "s"}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        <p className='fs-7'><i className='bx bx-calendar'></i> {getDateRangeInPlainWithMonth(getDateRangeFromObject(item.from, item.to))} | {night} night{night > 1 && "s"}</p>
+                                        <p className='fs-7'><i className='bx bx-user'></i> {home.room} room{home.room > 1 && "s"}, {item.adult} adult{item.adult > 1 && "s"}{item.children > 0 && ", " + item.children + " child"}{item.children > 1 && "s"}</p>
                                     </div>
-                                    <p className='fs-7'><i className='bx bx-calendar'></i> {getDateRangeInPlainWithMonth(selectedDayRange)} | {night} night{night > 1 && "s"}</p>
-                                    <p className='fs-7'><i className='bx bx-user'></i> {home.room} room{home.room > 1 && "s"}, {home.adults} adult{home.adults > 1 && "s"}{home.child > 0 && ", " + home.child + " child"}{home.child > 1 && "s"}</p>
-                                </div>
-                                <div className="border-bottom">
-                                    <div className="row pt-3">
-                                        <p className='fw-semibold'><i className='bx bxs-building-house'></i>HOTEL <span className="fs-70 text-muted">(HO CHI MINH CITY)</span></p>
-                                    </div>
-                                    <div className="row g-0 m-2">
-                                        <div className="col-3">
-                                            <img src="https://picsum.photos/150/150" alt="" width={80} className="rounded-1" />
-                                        </div>
-                                        <div className="col-9">
-                                            <h5 className='fw-semibold'>NTA Serviced Apartments</h5>
-                                            <div>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                                <i className='bx bxs-star text-warning'></i>
-                                            </div>
-                                            <div className="row">
-                                                <div className="col-2 bg-main text-center rounded-2 p-1 m-2 mx-wh-30 fw-semibold text-white">10</div>
-                                                <div className="col-10 p-0 mt-1">
-                                                    <p className='m-0 fw-semibold fs-smaller'>Exceptional</p>
-                                                    <p className='fs-smaller'>From 300 reviews</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p className='fs-7'><i className='bx bx-calendar'></i> {getDateRangeInPlainWithMonth(selectedDayRange)} | {night} night{night > 1 && "s"}</p>
-                                    <p className='fs-7'><i className='bx bx-user'></i> {home.room} room{home.room > 1 && "s"}, {home.adults} adult{home.adults > 1 && "s"}{home.child > 0 && ", " + home.child + " child"}{home.child > 1 && "s"}</p>
-                                </div>
+                                ))}
                             </div>
                             <div className="row bg-white mt-3 rounded-2">
                                 <h4 className='my-3 fw-semibold'>Price details</h4>
+                                {chosenItems && chosenItems.map((item, index) => (
+                                    <div className="row m-0" key={index}>
+                                        <div className="col-6 text-start m-0 p-0">
+                                            <p className='fs-7 m-0'>{item.hotel.name}</p>
+                                            <p className='fs-smaller m-0'>{home.room} X room{home.room > 1 && "s"},{getNightNumber(getDateRangeFromObject(item.from, item.to))} X night{getNightNumber(getDateRangeFromObject(item.from, item.to)) > 1 && "s"}</p>
+                                            <p className='fs-smaller'>Taxes & fees</p>
+                                        </div>
+                                        <div className="col-6 text-end">
+                                            <p>
+                                                {Formatter.format(item.hotel.rooms[0].price)}
+                                            </p>
+                                            <p>
+                                                + {Formatter.format(item.hotel.rooms[0].price * 0.1)}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
 
-                                <div className="row m-0">
-                                    <div className="col-6 text-start m-0 p-0">
-                                        <p className='fs-7 m-0'>NTA Serviced Apartments</p>
-                                        <p className='fs-smaller m-0'>{home.room} X room{home.room > 1 && "s"},{night} X night{night > 1 && "s"}</p>
-                                        <p className='fs-smaller'>Taxes & fees</p>
-                                    </div>
-                                    <div className="col-6 text-end">
-                                        <p>
-                                            {Formatter.format(1234567)}
-                                        </p>
-                                        <p>
-                                            + {Formatter.format(1234567)}
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row m-0">
-                                    <div className="col-6 text-start m-0 p-0 ">
-                                        <p className='fs-7 m-0'>NTA Serviced Apartments</p>
-                                        <p className='fs-smaller m-0'>{home.room} X room{home.room > 1 && "s"},{night} X night{night > 1 && "s"}</p>
-                                        <p className='fs-smaller'>Taxes & fees</p>
-                                    </div>
-                                    <div className="col-6 text-end">
-                                        <p>
-                                            {Formatter.format(1234567)}
-                                        </p>
-                                        <p>
-                                            + {Formatter.format(1234567)}
-                                        </p>
-                                    </div>
-                                </div>
                                 <div className="row border-bottom w-95 m-2 mt-0"></div>
                                 <div className="row">
                                     <div className="col-6">
@@ -176,7 +189,7 @@ function Checkout(props) {
                                         <p className='fs-smaller'>with taxes & fees</p>
                                     </div>
                                     <div className="col-6 text-end m-0 p-0">
-                                        <p className="fs-smaller">{Formatter.format(12345678)}</p>
+                                        <p className="fs-smaller">{Formatter.format(total)}</p>
                                     </div>
                                 </div>
                                 <div className="row border-bottom w-95 m-2 mt-0"></div>
@@ -185,7 +198,7 @@ function Checkout(props) {
                                         <p className='fs-smaller m-0'>Total you pay</p>
                                     </div>
                                     <div className="col-6 text-end m-0 p-0">
-                                        <p className="fs-smaller">{Formatter.format(12345678 + 1234567)}</p>
+                                        <p className="fs-smaller">{Formatter.format(total)}</p>
                                     </div>
                                 </div>
                             </div>
